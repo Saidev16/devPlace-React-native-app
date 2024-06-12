62aaf69a5838ba43cad06111ec0bd71843d1e68f
saidev16 <boutouditsaid@gmail.com>
2024-06-12 23:35:47 +0100

    more of habit card

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -7,7 +7,7 @@ export default {
     purple: "#5F6CE2",
     lightPurple: "#dbdfff",
     dark: "#1F1F1F",
-    gray: "#e1e3f0",
+    gray: "#7d7d7d",
     text: "#1c1c1c",
     background: "#fcfcfc",
     lightGrey: "#ededed",
